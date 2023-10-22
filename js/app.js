afd6ba70c212fca1f723f263c5f02159913976d2
Muhamad Royhan Fadhli <122357726+MRoyhanF@users.noreply.github.com>
2023-10-22 19:18:43 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+function validateForm(){
+//      // event.preventDevault();
+//      //get input value
+//      // const name = document.forms['message-form']['full-name'].value;
+//      // const email = document.forms['message-form']['email'].value;
+//      // const hp = document.forms['message-form']['hp'].value;
+//      // const semester = document.forms['message-form']['semester'].value;
+//      // const ipk = document.forms['message-form']['ipk'].value;
+//      // const jurusan = document.forms['message-form']['jurusan'].value;
+//      // const berkas = document.forms['message-form']['berkas'].value;
+
+//      //const name = document.getElementById("name").value;
+//      //const email = document.getElementById("email").value;
+//      // const hp = document.getElementById("hp").value;
+//      // const semester = document.getElementById("semester").value;
+//      // const ipk = document.getElementById("ipk").value;
+//      // const jurusan = document.getElementById("jurusan").value;
+//      // const berkas = document.getElementById("berkas").value;
+//      // Simpan data di sessionStorage
+//      sessionStorage.setItem("name", name);
+//      sessionStorage.setItem("email", email);
+//      sessionStorage.setItem("hp", hp);
+//      sessionStorage.setItem("semester", semester);
+//      sessionStorage.setItem("ipk", ipk);
+//      sessionStorage.setItem("jurusan", jurusan);
+//      sessionStorage.setItem("berkas", berkas);
+
+//      // window.location.href = "display.html";
+
+//          // Ambil data dari sessionStorage dan tampilkan
+//     const name = sessionStorage.getItem("name");
+//     const email = sessionStorage.getItem("email");
+//     const hp = sessionStorage.getItem("hp");
+//     const semester = sessionStorage.getItem("semester");
+//     const ipk = sessionStorage.getItem("ipk");
+//     const jurusan = sessionStorage.getItem("jurusan");
+//     const berkas = sessionStorage.getItem("berkas");
+
+//     document.getElementById("displayName").textContent = name;
+//     document.getElementById("displayEmail").textContent = email;
+//     document.getElementById("displayEmail").textContent = hp;
+//     document.getElementById("displayEmail").textContent = semester;
+//     document.getElementById("displayEmail").textContent = ipk;
+//     document.getElementById("displayEmail").textContent = jurusan;
+//     document.getElementById("displayEmail").textContent = berkas;
+
+
+
+
+//      //validate input
+//      // if (name == '' || email == '' || hp == '' || ipk == '') {
+//      //      alert('Input tidak boleh kosong');
+//      //      return false;
+//      // }
+//      // //menampilkan value input  di halaman lain
+//      // document.getElementById('sender-full-name').innerText = name;
+//      // document.getElementById('sender-email').innerText = email;
+//      // document.getElementById('sender-hp').innerText = hp;
+//      // document.getElementById('sender-semester').innerText = semester;
+//      // document.getElementById('sender-ipk').innerText = ipk;
+//      // document.getElementById('sender-jurusan').innerText = jurusan;
+//      // document.getElementById('sender-berkas').innerText = berkas;
+//      // return false;
+
+
+}
